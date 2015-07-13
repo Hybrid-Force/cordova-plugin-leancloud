84b56d65fdc3da5a7af084291fd183c5ffb2166c
Xinyu Zhang <beviszhang1993@gmail.com>
2015-07-13 13:16:06 +0800

    add gitignore, add stuff to lpush.js

--- other/lpush.js
+++ other/lpush.js
@@ -1,24 +1,26 @@
 function ___cordovaLeanPushCallback___(x) {
     var notice;
-    if(typeof x == 'string'){
+    if (typeof x == 'string') {
         notice = JSON.parse(x);
-    }
-    else {
+    } else {
         notice = x;
     }
     if (notice) {
-        var $body = angular.element(document.body);
-        $body.scope().$root.$emit('leancloud:notificationRecieved', notice);
+        if (typeof angular != 'undefined') {
+            var $body = angular.element(document.body);
+            $body.scope().$root.$emit('leancloud:notificationRecieved', notice);
+        }
+        (window.LeanPush.onMessage._fun || function() {})(notice);
     }
 }
 
 function lpushInit() {
-    if (typeof angular == 'undefined') {
-        throw new Error("angular is not defined!");
-    }
-    if(typeof cordova == 'undefined'){
+    if (typeof cordova == 'undefined') {
         throw new Error("cordova is not defined!");
     }
+    window.LeanPush.onMessage = function(f) {
+        window.LeanPush.onMessage._fun = f;
+    };
     cordova.exec(___cordovaLeanPushCallback___, null, "LeanPush", "getCacheResult", []);
     cordova.exec(null, null, "LeanPush", "onMessage", [___cordovaLeanPushCallback___.name]);
 }
